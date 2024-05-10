c16aa2030cf1f0d2a334100afd02b24a67bc5db6
ValentynNVS <novosidlak3@gmail.com>
2024-05-10 17:34:44 -0400

    added a fucntion comments as well as file header comment

--- DS Focused 1/f1.cpp
+++ DS Focused 1/f1.cpp
@@ -1,3 +1,11 @@
+/*
+* FILE : f1.cpp
+* PROJECT : Focused 1 assignment
+* PROGRAMMER : Valentyn Novosydliuk
+* FIRST VERSION : 05/10/2024
+* DESCRIPTION :
+* This program gets user flight infomation and aims to work with active memory allocation using malloc();
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,12 +40,28 @@ int main(void) {
 }
 
 
+/*
+Function: fillFlightInfo
+Parameters: struct FlightInfo* flight - a pointer to a struct FlightInfo (which is a
+										pointer to an element of the array)
+			const char* destination - constant pointer to flight information string
+			const char* date - constant pointer to flight information string
+Description: Thhis fucntion fills in the struct field and allocates memory blocks
+Return value: void
+*/
 void fillFlightInfo(struct FlightInfo* flight, const char* destination, const char* date) {
 
 
 
 }
 
+
+/*
+Function: printfFlightInfo
+Parameters: struct FlightInfo* flight - a pointer to a struct FlightInfo
+Description: This functions prints the formatted output after the fillFlightInfo
+Return value: void
+*/
 void printFlightInfo(struct FlightInfo* flights) {
 
 	
