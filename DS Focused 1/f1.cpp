0bbb29f8660050b6fa9c05ccfb64dff62e8ded49
ValentynNVS <novosidlak3@gmail.com>
2024-05-10 17:20:17 -0400

    created struct to store the destinations, also created constant values and a for loop where the main part of program will be held

--- DS Focused 1/f1.cpp
+++ DS Focused 1/f1.cpp
@@ -2,9 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+const int kMaxStrSize = 30;
+const int kNumberOfFlights = 10;
+
+
+struct FlightInfo {
+
+	char* destination;
+	char* date;
+
+};
+
 int main(void) {
 
+	char destination[kMaxStrSize] = "";
+	char dateOfTheFlight[kMaxStrSize] = "";
 
+	struct FlightInfo flight[kNumberOfFlights];
+
+	for (int i = 0; i < kNumberOfFlights; i++) {
+
+
+	}
 
 	return 0;
 }
